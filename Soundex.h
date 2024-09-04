c997bae0c973d69a3c76b29e52b3ceedc82f9464
Vijay K S <57295466+vijay1421@users.noreply.github.com>
2024-09-04 22:17:02 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -6,33 +6,32 @@
 #include <string.h>
 
 char getSoundexCode(char c) {
-    static const char codeTable[26] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
+    static const char alphabetToNumberTable[26] = {
+        '0', '1', '2', '3', '0', '1', '2', '0', '0','2', '2', '4', '5', '5', '0', '1', '2', '6',  '2', '3', '0', '1', '0', '2', '0', '2'     
     };
+     c = toupper(c);
     if (isalpha(c)) {
-        return codeTable[toupper(c) - 'A'];
+        return alphabetToNumberTable[c - 'A'];
     }
     return '0';
-}
-
-// Function to update the Soundex code array
+}  
+ 
 int updateSoundex(char code, int sIndex, char *soundex) {
-    if (code != '0') {
+    int notZero = code != '0';
+    if (notZero) {
         soundex[sIndex] = code;
-        return sIndex + 1;
-    }
+        return ++sIndex;
+    } 
+    soundex[sIndex] = soundex[sIndex];
     return sIndex;
 }
-
-// Function to generate the Soundex code from a name
 void generateSoundex(const char *name, char *soundex) {
-    if (name[0] != '\0') {
-        soundex[0] = toupper(name[0]);
-    }
+    soundex[0] = toupper(name[0]);
     int sIndex = 1;
-
-    for (int i = 1; name[i] != '\0' && sIndex <= 3; ++i) {
-        sIndex = updateSoundex(getSoundexCode(name[i]), sIndex, soundex);
+ 
+     for (int i = 1; name[i] != '\0' && sIndex <=3; i++) {
+        char code = getSoundexCode(name[i]);
+        sIndex = updateSoundex(code, sIndex, soundex); 
     }
     memset(soundex + sIndex, '0', 4 - sIndex);
     soundex[4] = '\0';
