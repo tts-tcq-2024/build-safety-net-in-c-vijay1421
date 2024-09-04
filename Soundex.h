086a3d0c622aac0d3587e92f8e6362c800b76e60
Vijay K S <57295466+vijay1421@users.noreply.github.com>
2024-09-04 22:34:00 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,52 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
- 
+
+#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
-#include <stdio.h>
- 
-char getSoundexCode(char c) {
-    static const char codeTable[26] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0','2', '2', '4', '5', '5', '0', '1', '2', '6',  '2', '3', '0', '1', '0', '2', '0', '2'     
-    };
-     c = toupper(c);
+
+// Function to get the Soundex code for a character
+char alphabetsToCode(char c) {
+    static const char alphabetsCode[26] = {'0', '1', '2', '3','0',  '1',  '2',  '0','0',  '2',  '2', '4','5', '5', '0',  '1', '2', '6','2', '3','0', '1',  '0', '2','0', '2'};
+
+    c = toupper(c);
     if (isalpha(c)) {
-        return codeTable[c - 'A'];
+        return alphabetsCode[c - 'A'];
     }
     return '0';
-}  
- 
-int updateSoundex(char code, int sIndex, char *soundex) {
-    int notZero = code != '0';
-    if (notZero) {
-        soundex[sIndex] = code;
-        return ++sIndex;
-    } 
-    soundex[sIndex] = soundex[sIndex];
-    return sIndex;
 }
-void generateSoundex(const char *name, char *soundex) {
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
- 
-     for (int i = 1; name[i] != '\0' && sIndex <=3; i++) {
-        char code = getSoundexCode(name[i]);
-        sIndex = updateSoundex(code, sIndex, soundex); 
+void initializeSoundex(char *soundex, char firstLetter) {
+    soundex[0] = toupper(firstLetter);
+}
+
+void handleAlphabets(char currentChar, char *soundex, int *sIndex, char *previousCode) {
+    char code = alphabetsToCode(currentChar);
+    if (code != '0' && code != *previousCode) {
+        soundex[(*sIndex)++] = code;
+        *previousCode = code;
     }
-    memset(soundex + sIndex, '0', 4 - sIndex);
+}
+
+void processAlphabets(const char *name, char *soundex, int *sIndex, char *previousCode) {
+    for (int i = 1; name[i] != '\0' && *sIndex < 4; i++) {
+        handleAlphabets(name[i], soundex, sIndex, previousCode);
+    }
+}
+
+void fillEmptyWithZeroes(char *soundex, int *sIndex) {
+    while (*sIndex < 4) {
+        soundex[(*sIndex)++] = '0';
+    }
+}
+
+void generateSoundex(const char *name, char *soundex) {
+    initializeSoundex(soundex, name[0]);
+    char previousCode = alphabetsToCode(name[0]);
+    int sIndex = 1;
+
+    processAlphabets(name, soundex, &sIndex, &previousCode);
+    fillEmptyWithZeroes(soundex, &sIndex);
+
     soundex[4] = '\0';
 }
- 
-#endif // SOUNDEX_H
+#endif
