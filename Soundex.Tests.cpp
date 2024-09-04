e8fe54bf29f8874e6cfc6cd1893e653ebebe540a
Vijay K S <57295466+vijay1421@users.noreply.github.com>
2024-09-04 22:14:37 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,12 +2,43 @@
 #include "Soundex.h"
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
+//AAA
   char soundex[5];
-  generateSoundex("AX", soundex);
- ASSERT_EQ(soundex,"A200");
-
- char soundex[5];
-generateSoundex("Vijay", soundex);
-ASSERT_STREQ(soundex,"V000");
+generateSoundex("$rrr", soundex);
+ASSERT_STREQ(soundex,"$666");
 }
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+//AAA
+char soundex[5];
+generateSoundex("pvt", soundex);
+ASSERT_STREQ(soundex,"P130");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+//AAA
+  char soundex[5];
+generateSoundex("*#12hi", soundex);
+ASSERT_STREQ(soundex,"*000");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_vowels) {
+//AAA
+char soundex[5];
+generateSoundex("aeiou", soundex);
+ASSERT_STREQ(soundex,"A000");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_space) {
+//AAA
+char soundex[5];
+generateSoundex(" ", soundex);
+ASSERT_STREQ(soundex," 000");
+}
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+//AAA
+char soundex[5];
+generateSoundex("bcdlmnr", soundex);
+ASSERT_STREQ(soundex,"B234");
+
