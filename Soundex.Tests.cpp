9b476b6f7868b113679c3d5fd19e843694f1ac4b
Vijay K S <57295466+vijay1421@users.noreply.github.com>
2024-09-05 15:39:42 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -8,29 +8,21 @@
 void testGenerateSoundex() {
     char soundex[5];
 
-    // Test case 1: name
     generateSoundex("Vijay", soundex);
     assert(strcmp(soundex, "V200") == 0);
-
-    // Test case 2: Name with repeated leyters
+    
     generateSoundex("Banana", soundex);
     assert(strcmp(soundex, "B500") == 0);
 
-    // Test case 3: Name with mixed vowels and consonants
     generateSoundex("Rules", soundex);
     assert(strcmp(soundex, "R420") == 0);
 
-    // Test case 4: Short name
     generateSoundex("Bee", soundex);
     assert(strcmp(soundex, "B000") == 0);
 
-    printf("All test cases passed successfully!\n");
-
-    // Test case 5: Name with non-alphabetic characters
     generateSoundex("O'Connor", soundex);
     assert(strcmp(soundex, "O256") == 0);
 
-    // Test case 6: Name with all vowels
     generateSoundex("AEIOU", soundex);
     assert(strcmp(soundex, "A000") == 0);
 }
